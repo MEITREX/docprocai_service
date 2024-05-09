37e8e77e501a05a84b9fa1e8cee1be7625f0a1fb
Rick Zolnierek <sjatah@gmail.com>
2024-05-09 12:42:44 +0200

    Added webvtt

--- src/fileextractlib/LectureVideoProcessor.py
+++ src/fileextractlib/LectureVideoProcessor.py
@@ -3,23 +3,49 @@ import time
 from os import path
 from datetime import timedelta
 
+from webvtt import WebVTT, Caption
+
+
 class LectureVideoProcessor:
     def __init__(self):
         self.model: whisper.Whisper = whisper.load_model(name="base")
 
     def process(self, audio_file: str) -> str:
         start_time: float = time.time()
-        result  = model.transcribe(audio=audio_file, word_timestamps=True)
+        result = self.model.transcribe(audio=audio_file, word_timestamps=True)
         end_time: float = time.time()
 
         result_text: str = ""
+
+        vtt = WebVTT()
 
         for segment in result["segments"]:
             segment_text: str = ""
             for word in segment["words"]:
                 segment_text += word["word"] + " "
-    
+
+            segment_start = timedelta(seconds=segment["words"][0]["start"])
+            segment_end = timedelta(seconds=segment["words"][-1]["end"])
+
+            if segment_start.microseconds == 0:
+                segment_start = segment_start + timedelta(microseconds=1)
+
+            if segment_end.microseconds == 0:
+                segment_end = segment_end + timedelta(microseconds=1)
+
+            caption = Caption(
+                str(segment_start),
+                str(segment_end),
+                "-" + segment_text
+            )
+            vtt.captions.append(caption)
+            vtt.save("test.vtt")
             result_text += str(timedelta(seconds=segment["words"][0]["start"])) + " -- " + segment_text + "\n"
 
-        print("Processed text in " + str(end_time - start_time) +  " seconds.")
+        print("Processed text in " + str(end_time - start_time) + " seconds.")
         return result_text
+
+
+if __name__ == "__main__":
+    processor = LectureVideoProcessor()
+    processor.process("E:\Programmiertes\FoPro\Test.wav")
