e352d501aa4c1b4bcb1f1dc0df785c9ab92cb6ab
Valentin Morlock <valentin@morlock.io>
2024-05-22 15:33:02 +0200

    bring pdf-ocr to a nicer format

--- src/fileextractlib/Pdf.py
+++ src/fileextractlib/Pdf.py
@@ -1,19 +1,8 @@
 import os
-from PIL import Image
 from pdf2image import convert_from_path
 import pytesseract
-
-import whisper
-import time
 from os import path
 import argparse
-from datetime import timedelta
-import numpy as np
-import ffmpeg
-import io
-
-from webvtt import WebVTT, Caption
-
 
 class PdfProcessor:
     """
@@ -24,42 +13,15 @@ class PdfProcessor:
 
             
         doc = convert_from_path(file_name)
-        
-        path, fileName = os.path.split(file_name)
-        fileBaseName, fileExtension = os.path.splitext(fileName)
         os.environ['OMP_THREAD_LIMIT'] = '20'
 
+        pages = []
+
         for page_number, page_data in enumerate(doc):
-            txt = pytesseract.image_to_string(page_data).encode("utf-8")
-            print("Page # {} - {}".format(str(page_number),txt))
-
-        for segment in result["segments"]:
-            segment_text: str = ""
-            for word in segment["words"]:
-                segment_text += word["word"]
-
-            segment_start = timedelta(seconds=segment["words"][0]["start"])
-            segment_end = timedelta(seconds=segment["words"][-1]["end"])
-
-            if segment_start.microseconds == 0:
-                segment_start = segment_start + timedelta(microseconds=1)
-
-            if segment_end.microseconds == 0:
-                segment_end = segment_end + timedelta(microseconds=1)
-
-            caption = Caption(
-                str(segment_start),
-                str(segment_end),
-                "-" + segment_text
-            )
-            vtt.captions.append(caption)
-
-        print("Processed text in " + str(end_time - start_time) + " seconds.")
+            text = pytesseract.image_to_string(page_data).encode("utf-8")
+            pages.append({page_number, text})
         
-        with io.StringIO() as f:
-            vtt.write(f)
-            f.seek(0)
-            return f.read() 
+        return pages
 
 
 if __name__ == "__main__":
