5dda54450ce09bc2c0d94dc938d6b151c2c3c7f7
Rick Zolnierek <sjatah@gmail.com>
2024-09-30 18:19:37 +0200

    added basic topic modeling

--- fileextractlib/TopicModel.py
+++ fileextractlib/TopicModel.py
@@ -1,73 +1,58 @@
-import argparse
-import json
-import os
-from pathlib import Path
-
+import numpy as np
 from bertopic import BERTopic
-from sklearn.feature_extraction.text import CountVectorizer
 from bertopic.vectorizers import ClassTfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 
-import PdfProcessor as PdfProcessor
-
-docs = []
-
-
-def createTopicModel():
-    vectorizer_model = CountVectorizer(stop_words="english", ngram_range=(1, 3))
-    ctfidf_model = ClassTfidfTransformer(reduce_frequent_words=True, bm25_weighting=True)
-
-    topic_model = BERTopic(
-        vectorizer_model=vectorizer_model,
-        ctfidf_model=ctfidf_model
-    )
-    topics = topic_model.fit_transform(docs)
-    print(topic_model.get_topic_info())
-    print(topic_model.get_topic(0, full=True))
-
-    fig = topic_model.visualize_topics()
-    fig.write_html("topicmodel.html")
-
-    fig2 = topic_model.visualize_barchart(top_n_topics=50, n_words=100)
-    fig2.write_html("barchart.html")
-
-    fig3 = topic_model.visualize_documents(docs)
-    fig3.write_html("docs.html")
-
-    return topics
+from persistence.DbConnector import DbConnector
+import dto.mapper as mapper
+from persistence.entities import DocumentSegmentEntity, VideoSegmentEntity
 
 
-def addFileToDocs(file, filetype):
-    if filetype == "pdf":
-        pdf_processor = PdfProcessor.PdfProcessor()
-        pages = pdf_processor.process_from_path(file)
-        filtered_pages = [x for x in pages if x["text"] is not None and x["text"].strip()]
+class TopicModel:
+    def __init__(self):
+        self.database = DbConnector(
+            conn_info="user=root password=root host=localhost port=5431 dbname=docprocai_service")
 
-        docs.append(json.dumps(filtered_pages))
+    def createTopicModel(self):
+        query_results = self.database.get_all_record_segments()
 
-    elif filetype == "json":
-        with open(file, encoding="utf8") as f:
-            data = json.load(f)
-            for x in data:
-                docs.append(x.get('transcript'))
+        docs = []
+        embeddings = []
+
+        for entity in query_results:
+            if isinstance(entity, DocumentSegmentEntity):
+                docs.append(entity.text)
+                embeddings.append(entity.embedding)
+            if isinstance(entity, VideoSegmentEntity):
+                docs.append(entity.transcript)
+                embeddings.append(entity.embedding)
+
+        embeddings = np.array(embeddings)
+
+        vectorizer_model = CountVectorizer(stop_words="english", ngram_range=(1, 3))
+        ctfidf_model = ClassTfidfTransformer(reduce_frequent_words=True, bm25_weighting=True)
+
+        model = BERTopic(
+            vectorizer_model=vectorizer_model,
+            ctfidf_model=ctfidf_model
+        )
+        print("Running topic model")
+        topics = model.fit_transform(docs, embeddings)
+        print("Finished running model")
+
+        print(model.get_topic_info())
+        print(model.get_topic(0, full=True))
+
+        fig = model.visualize_topics()
+        fig.write_html("topicmodel.html")
+
+        fig2 = model.visualize_barchart(top_n_topics=50, n_words=100)
+        fig2.write_html("barchart.html")
+
+        return topics
 
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()
-    #
-    # parser.add_argument('directory', nargs='+')
-    # args = parser.parse_args()
-    #
-    # cwd = Path.cwd()
-    #
-    # print(cwd)
-    # trainingdata_path = args.directory
+    topic_model = TopicModel()
 
-    # print(trainingdata_path)
-
-    path = "E:/Programmiertes/FoPro/fileextractlib/files/trainingdata"
-    files = filter(lambda filepath: filepath.is_file(), Path(path).glob('*'))
-    for file in files:
-        print(file)
-        addFileToDocs(file, "json")
-
-    createTopicModel()
+    topic_model.createTopicModel()
