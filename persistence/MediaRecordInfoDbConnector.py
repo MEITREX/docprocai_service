f0103f27a55ade8ed404c0c63fcf7d0b285990d7
myluki2000 <lukas636@live.com>
2024-10-07 14:22:25 +0200

    DbConnector refactoring into separate classes

---
+++ persistence/MediaRecordInfoDbConnector.py
@@ -0,0 +1,56 @@
+from typing import Optional
+from uuid import UUID
+
+import psycopg
+from pgvector.psycopg import register_vector
+
+
+class MediaRecordInfoDbConnector:
+    def __init__(self, db_connection: psycopg.Connection):
+        self.db_connection = db_connection
+
+        # ensure pgvector extension is installed, we need it to store text embeddings
+        self.db_connection.execute("CREATE EXTENSION IF NOT EXISTS vector")
+        register_vector(self.db_connection)
+
+        self.db_connection.execute(
+            """
+            CREATE TABLE IF NOT EXISTS media_records (
+              id uuid PRIMARY KEY,
+              summary text[],
+              vtt text
+            );
+            """)
+
+    def upsert_media_record_info(self, id: UUID, summary: list[str], vtt: Optional[str]):
+        self.db_connection.execute(
+            query="""
+                  INSERT INTO media_records (id, summary, vtt)
+                  VALUES (%s, %s, %s)
+                  ON CONFLICT (id)
+                  DO UPDATE SET
+                      summary = EXCLUDED.summary,
+                      vtt = EXCLUDED.vtt;
+                  """,
+            params=(id, summary, vtt)
+        )
+
+    def get_media_record_summary_by_media_record_id(self, media_record_id) -> list[str]:
+        query_result = self.db_connection.execute(
+            "SELECT summary FROM media_records WHERE media_record_id = %s",
+            (media_record_id,)).fetchone()
+
+        if query_result is None:
+            return []
+
+        return query_result["summary"]
+
+    def get_video_captions_by_media_record_id(self, media_record_id: UUID) -> str | None:
+        query_result = self.db_connection.execute(
+            "SELECT vtt FROM media_records WHERE id = %s",
+            (media_record_id,)).fetchone()
+
+        if query_result is None:
+            return None
+
+        return query_result["vtt"]
--- persistence/SegmentDbConnector.py
+++ persistence/SegmentDbConnector.py
@@ -3,67 +3,17 @@ from typing import Optional
 import psycopg
 
 from pgvector.psycopg import register_vector
-from psycopg.types.enum import register_enum, EnumInfo
 
 from persistence.entities import *
 
 
-class DbConnector:
-    def __init__(self, conn_info: str):
-        self.db_connection: psycopg.Connection = psycopg.connect(
-            conn_info,
-            autocommit=True,
-            row_factory=psycopg.rows.dict_row
-        )
+class SegmentDbConnector:
+    def __init__(self, db_connection: psycopg.Connection):
+        self.db_connection: psycopg.Connection = db_connection
 
         # ensure pgvector extension is installed, we need it to store text embeddings
         self.db_connection.execute("CREATE EXTENSION IF NOT EXISTS vector")
         register_vector(self.db_connection)
-
-        self.db_connection.execute(
-            """
-            DO $$ BEGIN
-                CREATE TYPE ingestion_state AS ENUM (
-                  'ENQUEUED',
-                  'PROCESSING',
-                  'DONE'
-                );
-            EXCEPTION
-                WHEN duplicate_object THEN null;
-            END $$;
-            """)
-        info = EnumInfo.fetch(self.db_connection, "ingestion_state")
-        register_enum(info, self.db_connection, IngestionStateDbType)
-
-        self.db_connection.execute(
-            """
-            DO $$ BEGIN
-                CREATE TYPE ingestion_entity_type AS ENUM (
-                  'MEDIA_RECORD',
-                  'CONTENT'
-                );
-            EXCEPTION
-                WHEN duplicate_object THEN null;
-            END $$;
-            """)
-        info = EnumInfo.fetch(self.db_connection, "ingestion_entity_type")
-        register_enum(info, self.db_connection, IngestionEntityTypeDbType)
-
-        self.db_connection.execute("""
-                                   CREATE TABLE IF NOT EXISTS media_record_ingestion_states (
-                                     id uuid PRIMARY KEY,
-                                     entity_type ingestion_entity_type,
-                                     state ingestion_state
-                                   );
-                                   """)
-
-        self.db_connection.execute("""
-                                   CREATE TABLE IF NOT EXISTS media_records (
-                                     id uuid PRIMARY KEY,
-                                     summary text[],
-                                     vtt text
-                                   );
-                                   """)
 
         # ensure database tables exist
         # table which contains the sections of all documents including their text, page number, and text embedding
@@ -101,55 +51,6 @@ class DbConnector:
                                    );
                                    """)
 
-    def upsert_entity_ingestion_info(self,
-                                     entity_id: UUID,
-                                     ingestion_entity_type: IngestionEntityTypeDbType,
-                                     ingestion_state: IngestionStateDbType) -> None:
-        self.db_connection.execute(
-            """
-            INSERT INTO media_record_ingestion_states (id, entity_type, state)
-            VALUES (%(id)s, %(entity_type)s, %(state)s)
-            ON CONFLICT(id)
-            DO UPDATE SET
-              entity_type = EXCLUDED.entity_type,
-              state = EXCLUDED.state;
-            """,
-            params={
-                "id": entity_id,
-                "entity_type": ingestion_entity_type,
-                "state": ingestion_state
-            })
-
-    def get_entities_ingestion_info(self, entity_ids: list[UUID]) -> list[EntityIngestionInfoEntity]:
-        """
-        Returns the ingestion states stored in the DB for the entities with the given IDs. Returned entity list may
-        not be in the same order as the passed IDs. If no entity exists in the DB with the given ID, it is excluded
-        from the returned list.
-        """
-        query_results = self.db_connection.execute(
-            """
-            SELECT id, state, entity_type
-            FROM media_record_ingestion_states
-            WHERE id = ANY(%s);
-            """,
-            params=(entity_ids,)).fetchall()
-
-        return [EntityIngestionInfoEntity(
-            entity_id=result["id"],
-            entity_type=result["entity_type"],
-            ingestion_state=result["state"]
-        ) for result in query_results]
-
-    def get_enqueued_or_processing_ingestion_entities(self) \
-            -> list[tuple[UUID, IngestionEntityTypeDbType, IngestionStateDbType]]:
-        query_results = self.db_connection.execute(
-            """
-            SELECT id, state, entity_type
-            FROM media_record_ingestion_states
-            WHERE state IN ('ENQUEUED', 'PROCESSING');
-            """).fetchall()
-        return [(x["id"], x["entity_type"], x["state"]) for x in query_results]
-
     def insert_document_segment(self, text: str, media_record_id: UUID, page_index: int,
                                 thumbnail: bytes, title: Optional[str], embedding: Tensor) -> None:
         self.db_connection.execute(
@@ -184,19 +85,6 @@ class DbConnector:
                   """,
             params=(content_id, segment1_id, segment2_id))
 
-    def upsert_media_record(self, id: UUID, summary: list[str], vtt: Optional[str]):
-        self.db_connection.execute(
-            query="""
-                  INSERT INTO media_records (id, summary, vtt)
-                  VALUES (%s, %s, %s)
-                  ON CONFLICT (id)
-                  DO UPDATE SET
-                      summary = EXCLUDED.summary,
-                      vtt = EXCLUDED.vtt;
-                  """,
-            params=(id, summary, vtt)
-        )
-
     def delete_media_record_segment_links_by_segment_ids(self, segment_ids: list[UUID]) -> list[MediaRecordSegmentLinkEntity]:
         query_result = self.db_connection.execute(
             """
@@ -206,7 +94,7 @@ class DbConnector:
                   """,
             {"segment_ids": segment_ids}).fetchall()
 
-        return [DbConnector.__media_record_segment_link_query_result_to_object(x) for x in query_result]
+        return [SegmentDbConnector.__media_record_segment_link_query_result_to_object(x) for x in query_result]
 
     def delete_media_record_segment_links_by_content_ids(self, content_ids: list[UUID]) \
             -> list[MediaRecordSegmentLinkEntity]:
@@ -216,7 +104,7 @@ class DbConnector:
             WHERE content_id = ANY(%(content_ids)s)
             RETURNING *;
             """, {"content_ids": content_ids}).fetchall()
-        return [DbConnector.__media_record_segment_link_query_result_to_object(x) for x in query_result]
+        return [SegmentDbConnector.__media_record_segment_link_query_result_to_object(x) for x in query_result]
 
     def delete_document_segments_by_media_record_id(self, media_record_ids: list[UUID]) -> list[DocumentSegmentEntity]:
         query_results = self.db_connection.execute(
@@ -227,7 +115,7 @@ class DbConnector:
                   """,
             {"mediaRecordIds": media_record_ids})
 
-        return [DbConnector.__document_segment_query_result_to_object(x) for x in query_results]
+        return [SegmentDbConnector.__document_segment_query_result_to_object(x) for x in query_results]
 
     def delete_video_segments_by_media_record_id(self, media_record_ids: list[UUID]) -> list[VideoSegmentEntity]:
         query_results = self.db_connection.execute(
@@ -238,27 +126,7 @@ class DbConnector:
                   """,
             {"mediaRecordIds": media_record_ids})
 
-        return [DbConnector.__video_segment_query_result_to_object(x) for x in query_results]
-
-    def get_media_record_summary_by_media_record_id(self, media_record_id) -> list[str]:
-        query_result = self.db_connection.execute(
-            "SELECT summary FROM media_records WHERE media_record_id = %s",
-            (media_record_id,)).fetchone()
-
-        if query_result is None:
-            return []
-
-        return query_result["summary"]
-
-    def get_video_captions_by_media_record_id(self, media_record_id: UUID) -> str | None:
-        query_result = self.db_connection.execute(
-            "SELECT vtt FROM media_records WHERE id = %s",
-            (media_record_id,)).fetchone()
-
-        if query_result is None:
-            return None
-
-        return query_result["vtt"]
+        return [SegmentDbConnector.__video_segment_query_result_to_object(x) for x in query_results]
 
     def get_segment_links_by_content_id(self, content_id: UUID) -> list[MediaRecordSegmentLinkEntity]:
         result = self.db_connection.execute("""
@@ -269,7 +137,7 @@ class DbConnector:
                     FROM media_record_links WHERE content_id = %s
                     """, (content_id,)).fetchall()
 
-        return [DbConnector.__media_record_segment_link_query_result_to_object(x) for x in result]
+        return [SegmentDbConnector.__media_record_segment_link_query_result_to_object(x) for x in result]
 
     def does_segment_link_exist(self, segment1_id: UUID, segment2_id: UUID, content_id: UUID = None) -> bool:
         if content_id is None:
@@ -351,7 +219,7 @@ class DbConnector:
 
         return [SemanticSearchResultEntity(
             x["score"],
-            DbConnector.__media_record_segment_query_result_to_object(x)
+            SegmentDbConnector.__media_record_segment_query_result_to_object(x)
         ) for x in query_results]
 
     def get_record_segments_by_media_record_ids(self, media_record_ids: list[UUID]) \
@@ -447,7 +315,7 @@ class DbConnector:
 
         entities = []
         for x in query_results:
-            entity = DbConnector.__media_record_segment_query_result_to_object(x)
+            entity = SegmentDbConnector.__media_record_segment_query_result_to_object(x)
             entities.append(entity)
 
         return entities
@@ -455,9 +323,9 @@ class DbConnector:
     @staticmethod
     def __media_record_segment_query_result_to_object(query_result) -> DocumentSegmentEntity | VideoSegmentEntity:
         if query_result["source"] == "document":
-            return DbConnector.__document_segment_query_result_to_object(query_result)
+            return SegmentDbConnector.__document_segment_query_result_to_object(query_result)
         elif query_result["source"] == "video":
-            return DbConnector.__video_segment_query_result_to_object(query_result)
+            return SegmentDbConnector.__video_segment_query_result_to_object(query_result)
 
     @staticmethod
     def __document_segment_query_result_to_object(query_result) -> DocumentSegmentEntity:
