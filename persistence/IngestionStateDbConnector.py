cb75a8c24005140919b26abda98777db6fd614d0
Rick Zolnierek <sjatah@gmail.com>
2024-10-27 13:30:39 +0100

    correct database name in query

--- persistence/IngestionStateDbConnector.py
+++ persistence/IngestionStateDbConnector.py
@@ -106,7 +106,7 @@ class IngestionStateDbConnector:
     def delete_ingestion_state(self, id: UUID) -> None:
         self.db_connection.execute(
             """
-            DELETE FROM ingestion_states WHERE id = ANY(%(id)s);
+            DELETE FROM media_record_ingestion_states WHERE id = ANY(%(id)s);
             """,
             {id: id}
         )
