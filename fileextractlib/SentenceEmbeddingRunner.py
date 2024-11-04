6d424cfe0d70c6ff8c621e30eb5b37a98ec6138a
myluki2000 <lukas636@live.com>
2024-11-04 18:54:40 +0100

    SentenceEmbeddingRunner: Do not force cuda

--- fileextractlib/SentenceEmbeddingRunner.py
+++ fileextractlib/SentenceEmbeddingRunner.py
@@ -4,9 +4,7 @@ import config
 class SentenceEmbeddingRunner:
     def __init__(self):
         self._model = SentenceTransformer(config.current["text_embedding"]["model_path"],
-                                          trust_remote_code=True,
-                                          device="cuda",
-                                          model_kwargs={"torch_dtype": "float16"})
+                                          trust_remote_code=True)
 
     def generate_embeddings(self, inputs: list[str]):
         embeddings = self._model.encode(inputs)
