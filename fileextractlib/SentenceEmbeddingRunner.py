24e7c9d43106d7e806d871bbc8bc65e35b3ddc22
myluki2000 <lukas636@live.com>
2025-09-03 16:27:46 +0200

    Change /embed query to POST

--- fileextractlib/SentenceEmbeddingRunner.py
+++ fileextractlib/SentenceEmbeddingRunner.py
@@ -14,10 +14,8 @@ class SentenceEmbeddingRunner:
         self.hostname = config.current["text_embedding"]["hostname"]
         self.port = config.current["text_embedding"]["port"]
 
-    def _create_url(self, words: List[str]) -> str:
-        query_params = [("words", word) for word in words]
-        query_string = urllib.parse.urlencode(query_params)
-        return f"{self.protocol}://{self.hostname}:{self.port}/embed?{query_string}"
+    def _create_url(self) -> str:
+        return f"{self.protocol}://{self.hostname}:{self.port}/embed"
 
     def generate_embeddings(self, words: List[str]) -> NDArray[np.float64]:
         """
@@ -25,7 +23,12 @@ class SentenceEmbeddingRunner:
         :param words: a list of words for which the respective embeddings shall be computed.
         :return: a list of embeddings vectors.
         """
-        response = requests.get(self._create_url(words))
+        response = requests.post(
+            self._create_url(),
+            json={
+                "words": words,
+            }
+        )
         response.raise_for_status()
         return np.array(response.json())
 
