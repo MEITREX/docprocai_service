0a0a97b154b42da8d7957cf48826605d18afd30c
myluki2000 <lukas636@live.com>
2025-09-03 16:39:50 +0200

    Make dapr http port environment variable configurable

--- events/DaprPublisher.py
+++ events/DaprPublisher.py
@@ -1,12 +1,14 @@
 import requests
 import json
+import os
 
 from events import MediaRecordInfoEvent
 
 PUBSUB_NAME = "meitrex"
 
 class DaprPublisher:
-    def __init__(self, dapr_port=9900):
+    def __init__(self):
+        dapr_port = os.environ.get("DAPR_HTTP_PORT", "3500")
         self.base_url = f"http://localhost:{dapr_port}/v1.0"
 
     def publish_media_record_info_event(self, event: MediaRecordInfoEvent):
